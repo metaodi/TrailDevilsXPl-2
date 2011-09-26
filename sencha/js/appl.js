74a698d92f894291456cbdcf6fda7bf8bad93d15
Stefan Oderbolz <oderbolz@gmail.com>
2011-09-26 09:36:54 +0200

    Twitter example app

---
+++ sencha/js/appl.js
@@ -0,0 +1,65 @@
+new Ext.Application({
+	tabletStartupScreen: 'phone_startup.png',
+	phoneStartupScreen: 'phone_startup.png',
+	icon: 'icon.jpg',
+	glossOnIcon: false,
+	onReady: function() {
+		var timeline, map, panel, tabBar, refresh;
+
+		timeline = new Ext.Component({
+			title: 'Timeline',
+			cls: 'timeline',
+			scroll: 'vertical',
+			tpl: [
+					'<tpl for=".">',
+					' <div class="tweet">',
+						' <div class="avatar"><img src="{profile_image_url}" /></div>',
+						' <div class="tweet-content">',
+							' <h2>{from_user}</h2>',
+							' <p>{text}</p>',
+						' </div>',
+					' </div>',
+					'</tpl>'
+				 ]
+		});
+	
+		panel = new Ext.TabPanel({
+			fullscreen: true,
+			cardSwitchAnimation: 'slide',
+			ui: 'light',
+			items: [timeline]
+		});
+	
+		panel.getTabBar().add([
+		       {xtype: 'spacer'},
+		       {
+		    	   xtype: 'button',
+		    	   iconMask: true,
+		    	   iconCls: 'refresh',
+		    	   ui: 'plain',
+		    	   style: 'margin:0;',
+		           handler: refresh
+		       }
+		]);
+		panel.getTabBar().doLayout();
+		
+		refresh = function() {
+			Ext.util.JSONP.request({
+				url: 'http://search.twitter.com/search.json',
+				callbackKey: 'callback',
+				params: {
+					q: "#hsr",
+					rpp: 30,
+					uniqueify: Math.random()
+				},
+				callback: function(data) {
+					var tweet_list = data.results;
+					timeline.update(tweet_list); // Update the tweets in timeline
+				}
+			});
+		};
+
+		refresh();
+	}
+});
+
