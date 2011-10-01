e65e95be7da53a6b20ee593133e90ab782469adb
tschortsch <juerg.hunziker@gmail.com>
2011-10-01 12:07:47 +0200

    pull refresh prototype

--- js/trailslist.js
+++ js/trailslist.js
@@ -49,11 +49,27 @@ function getTrailsList() {
 	registerTrailsListModel();
 	var trailsdata = getTrailsData();
 	
+	var pullRefresh = new Ext.plugins.PullRefreshPlugin({
+		pullRefreshText: 'Zum Aktualisieren herunterziehen',
+		releaseRefreshText: 'Zum Aktualisieren loslassen...',
+		loadingText: 'wird aktualisiert...',
+		pullTpl: new Ext.XTemplate(
+			'<div class="x-list-pullrefresh">',
+				'<div class="x-list-pullrefresh-arrow"></div>',
+				Ext.LoadingSpinner,
+				'<div class="x-list-pullrefresh-wrap">',
+					'<h3 class="x-list-pullrefresh-message">{message}</h3>',
+					'<div class="x-list-pullrefresh-updated">Zuletzt aktualisiert: {lastUpdated:date("d.m.Y H:i:s")}</span></div>',
+				'</div>',
+			'</div>'
+		)
+	});
+	
 	var trailslist = new Ext.List({
 	    fullscreen: true,
     	iconCls: 'maps',
         title: 'Trails',
-	    
+	    plugins: [pullRefresh],
         itemTpl: [
 		          '<div class="trail">',
 		          	'<h1>{title}</h1>',
