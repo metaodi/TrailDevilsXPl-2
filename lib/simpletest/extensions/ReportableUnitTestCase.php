aee0fa071ab9da5768cbe1f920a8f4dcd07de699
Stefan Oderbolz <oderbolz@gmail.com>
2011-10-05 11:32:55 +0200

    Split tests to report each Test separately

---
+++ lib/simpletest/extensions/ReportableUnitTestCase.php
@@ -0,0 +1,21 @@
+<?php
+require_once(dirname(__FILE__) . '/../unit_tester.php');
+/**
+ * Description of ReportableUnitTestCase
+ *
+ * @author odi
+ */
+class ReportableUnitTestCase extends UnitTestCase {
+	function report() 
+	{
+		$test = new TestSuite($this->getLabel());
+		$test->add($this);
+		if (TextReporter::inCli()) 
+		{
+			exit ($test->run(new JUnitXMLReporter()) ? 0 : 1);
+		} 
+		$test->run(new HtmlReporter());
+	}
+}
+
+?>
