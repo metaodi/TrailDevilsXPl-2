5a271dd638bb43a29513f3473cfc1274552743af
Stefan Oderbolz <oderbolz@gmail.com>
2011-10-17 14:28:02 +0200

    Traildevils-specific extensions for SimpleTest - show passes in HTML-Reporter - customer UnitTestCase-Class with some extra functionality for easier testing

---
+++ test/php/lib/simpletest/extensions/TraildevilsUnitTestCase.php
@@ -0,0 +1,52 @@
+<?php
+require_once(dirname(__FILE__) . '/../unit_tester.php');
+require_once('TraildevilsHTMLReporter.php');
+require_once('JUnitXMLReporter.php');
+/**
+ * Description of ReportableUnitTestCase
+ *
+ * @author odi
+ */
+abstract class TraildevilsUnitTestCase extends UnitTestCase {
+	function report() 
+	{
+		$test = new TestSuite($this->getLabel());
+		$test->add($this);
+		if (TextReporter::inCli()) 
+		{
+			exit ($test->run(new JUnitXMLReporter()) ? 0 : 1);
+		} 
+		$test->run(new TraildevilsHTMLReporter());
+	}
+	
+	function assertEqualsIgnoreWhitespace($first, $second, $message = '%s')
+	{
+		//ignore whitespace
+		$first = $this->removeWhitespace($first);
+		$second = $this->removeWhitespace($second);
+		
+		$this->assertEqual($first, $second, $message);
+	}
+	
+	private function removeWhitespace($text)
+	{
+		return preg_replace("/\s/", "", $text);
+	}
+	
+	function setUp() {
+		ob_start();
+    }
+
+    function tearDown() {
+        ob_end_flush();
+    }
+	
+	function getOutput()
+	{
+		$content = ob_get_contents();
+		//echo $content;
+		return $content;
+	}
+}
+
+?>
