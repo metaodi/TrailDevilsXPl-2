6916dcfe17c308e17c442a3406cedb25f26dabac
Stefan Oderbolz <oderbolz@gmail.com>
2011-10-26 16:40:36 +0200

    Added OData-Skip

--- php/DataLoader.class.php
+++ php/DataLoader.class.php
@@ -7,7 +7,7 @@ class DataLoader
 		echo $data;
 	}
 	
-	public function getTrails($url="http://152.96.80.18:8080/api/trails") /*http://jenkins.rdmr.ch/php/mock_api.json*/
+	public function getTrails($url="http://152.96.80.18:8080/api/trails?\$skip=10&\$top=10&\$orderby=TrailId") /*http://jenkins.rdmr.ch/php/mock_api.json*/
 	{
 		$remote = new JSONRemoteCaller($url);
 		return $this->convertJson($remote->callRemoteSite());
@@ -19,9 +19,9 @@ class DataLoader
 		for($i = 0; $i < count($externalTrailArray); $i++) {
 			$convertedArray[$i]["title"] = $externalTrailArray[$i]["Name"];
 			$convertedArray[$i]["location"] = ($externalTrailArray[$i]["NextCity"] ? $externalTrailArray[$i]["NextCity"].", " : "") . $externalTrailArray[$i]["Country"];
-			$convertedArray[$i]["distance"] = 100;
+			$convertedArray[$i]["distance"] = $i*100;
 			$convertedArray[$i]["thumb"] = $externalTrailArray[$i]["ImageUrl120"];
-			$convertedArray[$i]["description"] = $externalTrailArray[$i]["Desc"];
+			$convertedArray[$i]["description"] = nl2br($externalTrailArray[$i]["Desc"]);
 			$convertedArray[$i]["status"] = $externalTrailArray[$i]["IsOpen"] ? "offen" : "geschlossen";
 			$convertedArray[$i]["latitude"] = $externalTrailArray[$i]["GmapX"];
 			$convertedArray[$i]["longitude"] = $externalTrailArray[$i]["GmapY"];
