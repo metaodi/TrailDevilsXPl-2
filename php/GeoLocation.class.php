79145faff72e420a321c42e1af7304a30a84c59d
Stefan Oderbolz <oderbolz@gmail.com>
2011-10-31 19:49:00 +0100

    Modified distance algorithm

--- php/GeoLocation.class.php
+++ php/GeoLocation.class.php
@@ -30,18 +30,22 @@ class GeoLocation {
 
 	/* http://www.zipcodeworld.com/samples/distance.php.html */
 	public function distance(GeoLocation $to) {
-		$toLat = $to->getLatitude();
-		$toLng = $to->getLongitude();
+		$earthRadius = 6372797; // mean radius of Earth in meters
 		
-		$theta = $this->lng - $toLng;
-		$dist = sin(deg2rad($this->lat)) * sin(deg2rad($toLat)) + cos(deg2rad($this->lat)) * cos(deg2rad($toLat)) * cos(deg2rad($theta));
-		$dist = acos($dist);
-		$dist = rad2deg($dist);
+		$toLat = deg2rad($to->getLatitude());
+		$toLng = deg2rad($to->getLongitude());
+		$thisLat = deg2rad($this->lat);
+		$thisLng = deg2rad($this->lng);
 		
-		$meters = $dist * 60 * 1853.159616;
+		
+		$latDistance = $toLat - $thisLat;
+		$lngDistance = $toLng - $thisLng;
+		$distance = pow(sin($latDistance / 2),2) + cos($thisLat) * cos($toLat) * pow(sin($lngDistance / 2),2);
+		$distance = 2 * atan2(sqrt($distance), sqrt(1 - $distance));
+		$meters = $earthRadius * $distance;
+		
 		return $meters;
 	}
-
 }
 
 ?>
