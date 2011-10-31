e58fc9cfe246af14adcea0aaee71479174f03d0e
Juerg Hunziker <juerg.hunziker@gmail.com>
2011-10-31 21:25:35 +0100

    styled listpagingplugin

--- js/views/trailsList/TrailsListPagingPlugin.js
+++ js/views/trailsList/TrailsListPagingPlugin.js
@@ -5,7 +5,32 @@
  */
 
 traildevils.views.TrailsListPagingPlugin = Ext.extend(Ext.plugins.ListPagingPlugin, {
-	loadMoreText: 'weitere Trails laden...'
+	loadMoreText: 'Weitere Trails laden...',
+	loadingText: 'Trails werden geladen...',
+	
+	render : function() {
+        var list = this.list,
+            targetEl = list.getTargetEl(),
+            html = '';
+
+        if (!this.autoPaging) {
+            html += '<div class="x-list-paging-msg">' + this.loadMoreText + '</div>';
+        }
+
+        this.el = targetEl.createChild({
+            cls: 'x-list-paging' + (this.autoPaging ? ' x-loading' : ''),
+            html: html + Ext.LoadingSpinner + '<div class="x-list-paging-loadingmsg">' + this.loadingText + '</div>'
+        });
+
+        if (this.autoPaging) {
+            this.mon(targetEl.getScrollParent(), 'scrollend', this.onScrollEnd, this);
+        }
+        else {
+            this.mon(this.el, 'tap', this.onPagingTap, this);
+        }
+
+        this.rendered = true;
+    }
 });
 
 // Create ptype
