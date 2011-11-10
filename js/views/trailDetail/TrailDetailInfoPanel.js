7ea2a6c0e19bc0d888b12bab132816ed1a7589f2
Juerg Hunziker <juerg.hunziker@gmail.com>
2011-11-10 13:36:16 +0100

    bugfix for map not centered correctly problem

--- js/views/trailDetail/TrailDetailInfoPanel.js
+++ js/views/trailDetail/TrailDetailInfoPanel.js
@@ -20,7 +20,7 @@ traildevils.views.TrailDetailInfoPanel = Ext.extend(Ext.Panel, {
 		trailMapImageUrl += 'markers=icon:http://jenkins.rdmr.ch/resources/images/gmap_marker_cycling.png|' + this.data.latitude + ',' + this.data.longitude;
 		trailMapImageUrl += '&zoom=12&size=280x300&maptype=roadmap&sensor=false';
 		
-		var showOnMapLinkEvent = "Ext.dispatch({ controller: traildevils.controllers.trailsMapController, action: \'showtrailonmap\', latitude: ' + this.data.latitude + ', longitude: ' + this.data.longitude + ' });";
+		var showOnMapLinkEvent = "Ext.dispatch({ controller: traildevils.controllers.trailsMapController, action: \'showtrailonmap\', latitude: '" + this.data.latitude + "', longitude: '" + this.data.longitude + "' });";
 		
         this.tpl = new Ext.XTemplate(
 			'<div class="trail-detail">',
