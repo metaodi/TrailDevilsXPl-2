0aa1b35706e5271cd21945ca33b9e8e5ae138081
Stefan Oderbolz <oderbolz@gmail.com>
2011-11-28 17:27:19 +0100

    Extracted toRad() function to functions.js

--- js/application.js
+++ js/application.js
@@ -16,13 +16,6 @@ Ext.regApplication({
 	fullscreen: true,
 	
 	launch: function() {
-		/** Converts numeric degrees to radians */
-		if(typeof(Number.prototype.toRad) === "undefined") {
-			Number.prototype.toRad = function() {
-				return this * Math.PI / 180;
-			}
-		}
-		
 		// app is offline until told otherwise
 		traildevils.online = false;
 		
