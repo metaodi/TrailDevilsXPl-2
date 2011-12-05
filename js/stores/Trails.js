481a27779bbe0bcfcdd2c2b509dee239a713f3d9
Stefan Oderbolz <oderbolz@gmail.com>
2011-12-05 01:10:33 +0100

    Added workaround for sencha bug: init the records in exception block. - Fixes #30 - Fixes #53

--- js/stores/Trails.js
+++ js/stores/Trails.js
@@ -29,10 +29,12 @@ Ext.regStore('Trails', {
 				this.removeAllRecords();
 			}
 		},
-		load: function() {
-			traildevils.store.refreshData();
-			traildevils.online = true;
-			console.log('App ist online!');
+		load: function(store, records, successful) {
+			if (successful)	{
+				traildevils.store.refreshData();
+				traildevils.online = true;
+				console.log('App ist online!');
+			}
 		}
 	},
 	
@@ -49,9 +51,15 @@ Ext.regStore('Trails', {
 			//root: 'trails'
         },
         listeners: {
-			exception: function() {
+			exception: function(store, response, op) {
 				traildevils.online = false;
 				console.log('App ist offline!');
+				
+				/* WORKAROUND FOR SENCHA BUG: operation is not sucessful, therefore there are no records present
+				 * replace the undefined variable with an empty array to avoid "Uncaught TypeError: Cannot read property 'length' of undefined"
+				* Source: http://stackoverflow.com/questions/7119775/handling-404-exceptions-in-sencha-touch-store-with-an-ajax-proxy
+				*/
+				op.records = [];
 			}
         }
 	},
